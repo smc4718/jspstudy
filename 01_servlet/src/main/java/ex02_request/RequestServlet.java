20cf6995c9aa1a3a7cac2d9023da79e67123601b
smc4718 <smc4718@naver.com>
2023-09-08 14:49:05 +0900

    230908(5)

---
+++ 01_servlet/src/main/java/ex02_request/RequestServlet.java
@@ -0,0 +1,46 @@
+package ex02_request;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestServlet
+ */
+@WebServlet("/request")
+public class RequestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	   // getWriter() : 문자로 만들어 보내겠다.                ContextPath() : " /servlet " 이라고 텍스트가 빠져나온다.
+	   // 출력결과 : Served at: /servlet
+	  response.getWriter().append("안녕하세요.");
+	  String name = request.getParameter("name");
+	  System.out.println(name);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
