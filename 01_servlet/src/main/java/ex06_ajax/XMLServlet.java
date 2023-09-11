954aaa616cc78c6a0093d4344bb1d07619cbb6ee
smc4718 <smc4718@naver.com>
2023-09-11 15:34:52 +0900

    230911(6)

--- 01_servlet/src/main/java/ex06_ajax/XMLServlet.java
+++ 01_servlet/src/main/java/ex06_ajax/XMLServlet.java
@@ -1,11 +1,16 @@
 package ex06_ajax;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.json.XML;
 
 /**
  * Servlet implementation class XMLServlet
@@ -37,9 +42,37 @@ public class XMLServlet extends HttpServlet {
 	  if(strAge != null && !strAge.isEmpty()) {
 	    age = Integer.parseInt(strAge);
 	  }
+
+	  // 3. XML 만들기
+	  /*
+	   *  {"person": {"name:": "alice", "age": 30}}를 생성한 뒤 XML 태그 형식으로 변환
+	   *  <?xml version="1.0" encoding="UTF-8"?>
+	   *  <person>
+	   *     <name>alice</name>
+	   *     <age>30</age>
+	   *  </person>
+	   */
+
+	  JSONObject person = new JSONObject();
+	  person.put("name", name);
+	  person.put("age", age);
+	  JSONObject resJSON = new JSONObject();
+	  resJSON.put("person", person);
 	  
-	  System.out.println(name + ", " + age ); 
+	  String responseXML = XML.toString(resJSON);
 	  
+	  // 4. 응답 데이터 타입과 인코딩
+	  response.setContentType("application/xml; charset=UTF-8");
+	  
+	  // 5. 응답 스트림 생성
+	  PrintWriter out = response.getWriter();
+	  
+	  // 6. 응답
+	  out.println(responseXML);  // $.ajax({success: function(resData){}...)
+	                             // function의 매개변수 resData로 responseXML이 전달된다.
+
+	  out.flush();
+	  out.close();
 	}
 
 	/**
